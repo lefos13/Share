ee6708dd083f71dbbd4f169f8414c1ec5ab859c8
lefos Evan <lefterisevagelinos1996@gmail.com>
2023-02-07 18:22:38 +0200

    fix return filter

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -639,41 +639,63 @@ module.exports = {
             data.returnEndDate != null
               ? moment(data.returnEndDate)
               : moment(data.returnStartDate).add(1, "months");
-          // console.log("FOR POST: ", obj.post.postid);
-          // console.log("BEFORE start: ", obj.post.returnStartDate);
-          // console.log("BEFORE end: ", obj.post.returnEndDate);
-          // console.log("After start: ", postStartDate);
-          // console.log("After end: ", postEndDate);
-          // console.log("============");
-          // console.log("BEFORE search start: ", data.returnStartDate);
-          // console.log("BEFORE search end: ", data.returnEndDate);
-          // console.log("After search start: ", searchStartDate);
-          // console.log("After search end: ", searchEndDate);
 
           // case that the post has only a startreturndate
-          if (postEndDate == null) {
-            if (
-              postStartDate.isBetween(
-                searchStartDate,
-                searchEndDate,
-                null,
-                "[]"
+          if (data.returnEndDate == null) {
+            if (postEndDate == null) {
+              if (
+                postStartDate.isBetween(
+                  searchStartDate,
+                  searchEndDate,
+                  null,
+                  "[]"
+                )
               )
-            )
-              return true;
-          }
+                return true;
+            }
 
-          if (postEndDate != null) {
-            if (
-              postStartDate.isBetween(
-                searchStartDate,
-                searchEndDate,
-                null,
-                "[]"
-              ) ||
-              postEndDate.isBetween(searchStartDate, searchEndDate, null, "[]")
-            ) {
-              return true;
+            if (postEndDate != null) {
+              if (
+                searchStartDate.isBetween(
+                  postStartDate,
+                  postEndDate,
+                  null,
+                  "[]"
+                )
+              ) {
+                return true;
+              }
+            }
+          } else {
+            if (postEndDate == null) {
+              if (
+                postStartDate.isBetween(
+                  searchStartDate,
+                  searchEndDate,
+                  null,
+                  "[]"
+                )
+              )
+                return true;
+            }
+
+            if (postEndDate != null) {
+              if (
+                postStartDate.isBetween(
+                  searchStartDate,
+                  searchEndDate,
+                  null,
+                  "[]"
+                ) ||
+                postEndDate.isBetween(
+                  searchStartDate,
+                  searchEndDate,
+                  null,
+                  "[]"
+                )
+              ) {
+                return true;
+              }
             }
           }
 
